38352fd481429b13543dc1527ff018323303839b
Vahe <vah.aghajanyan@gmail.com>
2025-01-20 16:53:26 +0400

    1. LeetCode1929: Concatenation Of Array problem solved.

---
+++ src/A_ArraysAndHashing/Test.java
@@ -0,0 +1,15 @@
+package A_ArraysAndHashing;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        // Case 1
+        int[] nums = {1, 3, 2, 1};
+        System.out.println(Arrays.toString(ConcatenationOfArray.getConcatenation(nums)));
+
+        // Case 2
+        int[] nums2 = {1, 2, 3};
+        System.out.println(Arrays.toString(ConcatenationOfArray.getConcatenation(nums2)));
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import A_ArraysAndHashing.ConcatenationOfArray;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
